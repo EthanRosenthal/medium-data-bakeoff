8c777ddb830a611f996c88045ffe8321cf452f3e
Colin Sullivan <csullivan@brandwatch.com>
2023-08-17 11:00:27 -0400

    fixed swapped module names

--- src/medium_data_bakeoff/ingredients/dask_auto_optimized.py
+++ src/medium_data_bakeoff/ingredients/dask_auto_optimized.py
@@ -1,16 +1,15 @@
 import time
 
 import dask_expr as dd
-import ray
-from ray.util.dask import enable_dask_on_ray
+from dask.distributed import Client, LocalCluster
 
 
 def bake(dataset: str) -> float:
-    ray.init()
-    with enable_dask_on_ray():
+    with LocalCluster() as cluster:
+        client = Client(cluster)
         start = time.time()
         df = dd.read_parquet(dataset)
         df.groupby("station_id")["num_bikes_available"].mean().compute()
         stop = time.time()
-    ray.shutdown()
+        client.close()
     return stop - start
